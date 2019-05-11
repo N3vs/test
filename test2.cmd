fddb69d93484705565626870a399435cfd12dfa5
N3vs <50504352+N3vs@users.noreply.github.com>
2019-05-11 02:06:17 +0200

    Update test2.cmd

--- test2.cmd
+++ test2.cmd
@@ -1,5 +1,5 @@
 cd %homepath%\AppData\Local\Microsoft\Windows\Themes\RoamedThemeFiles\DesktopBackground
-powershell -Command "Invoke-WebRequest https://media.makeameme.org/created/haha-got-ya.jpg"
+powershell -Command "Invoke-WebRequest https://media.makeameme.org/created/haha-got-ya.jpg -UseBasicParsing -OutFile haha.jpg"
 
-reg add "HKCU\Control Panel\Desktop" /v Wallpaper /t REG_SZ /d "%cd%\haha-got-ya.jpg" /f
+reg add "HKCU\Control Panel\Desktop" /v Wallpaper /t REG_SZ /d "%cd%\haha.jpg" /f
 reg add "HKCU\Control Panel\Desktop" /v WallpaperStyle /t REG_SZ /d 0 /f
